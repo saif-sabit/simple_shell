ff8515d9b48a0218400fbf15634947fabb4f0d01
asimsharf <asimsharf@gmail.com>
2023-05-25 01:43:53 +0300

    update

--- shell.sh
+++ shell.sh
@@ -8,3 +8,5 @@ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o shell_0.3
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o shell_0.4.1
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o simple_shell
 sudo chmod +x shell shell_0.3 shell_0.4.1 simple_shell
+
+# valgrind –leak-check=full –show-leak-kinds=all ./shell 
